2252c7881ef6884f713620ed618f316d8e858aa0
ParkBoEun <ppos2eun@gmail.com>
2023-11-30 22:39:35 +0900

    Creating scvInfo class

--- scripts/sendHunter.py
+++ scripts/sendHunter.py
@@ -9,82 +9,72 @@ import nvidia_smi
 import socket
 import rosnode
 
-cpuPub = rospy.Publisher('/cpuPub', Float64, queue_size=10)
-ramPub = rospy.Publisher('/ramPub', Float64, queue_size=10)
-gpuPub = rospy.Publisher('/gpuPub', Float64, queue_size=10)
-scvIPPub = rospy.Publisher('/scvIPPub', String, queue_size=10)
-scvStatusPub = rospy.Publisher('/scvStatusPub', Float32MultiArray, queue_size=10)
-imgSamplingPub = rospy.Publisher('/imgSamplingPub', sensor_msgs.msg.CompressedImage, queue_size=10)
 
-def gpuUsage():
-    nvidia_smi.nvmlInit()
-    deviceCount = nvidia_smi.nvmlDeviceGetCount()
-    for i in range(deviceCount):
-        handle = nvidia_smi.nvmlDeviceGetHandleByIndex(i)
-        util = nvidia_smi.nvmlDeviceGetUtilizationRates(handle)
-        mem = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
-        print(
-            f"|Device {i}| Mem Free: {mem.free / 1024 ** 2:5.2f}MB / {mem.total / 1024 ** 2:5.2f}MB "
-            f"| gpu-util: {util.gpu / 100.0:3.1%} | gpu-mem: {util.memory / 100.0:3.1%} |")
-        gpuUsageValue = util.gpu
-        gpuMempercent = {util.memory / 100.0}
-        print(gpuUsageValue)
-        gpuPub.publish(gpuUsageValue)
+# class 예쁘게 만들기
 
-def cpuUsage():
-    # self.cpuUsageValue = cpuUsageValue
-    cpuUsageValue = psutil.cpu_percent()
-    print(f"CPU Percentage usage: {cpuUsageValue}%")
-    # CPU frequency
-    # cpu_info = psutil.cpu_freq()
-    # print(f"CPU Current frequency: {cpu_info.current:.2f} Mhz")
-    cpuPub.publish(cpuUsageValue)
+class scvInformation():
+    pubCpu = rospy.Publisher('/pubCpu', Float64, queue_size=10)
+    pubRam = rospy.Publisher('/pubRam', Float64, queue_size=10)
+    pubGpu = rospy.Publisher('/pubGpu', Float64, queue_size=10)
+    pubScvIP = rospy.Publisher('/pubScvIP', String, queue_size=10)
+    pubScvStatus = rospy.Publisher('/pubScvStatus', Float32MultiArray, queue_size=10)
 
-def ramUsage():
-    # self.ramUsageValue = ramUsageValue
-    ram_info = psutil.virtual_memory()
-    print(f"RAM Total: {ram_info.total / 1024 / 1024 / 1024:.2f} GB")
-    print(f"RAM Available: {ram_info.available / 1024 / 1024 / 1024:.2f} GB")
-    print(f"RAM Used: {ram_info.used / 1024 / 1024 / 1024:.2f} GB")
-    print(f"RAM Percentage usage: {ram_info.percent}%")
-    ramTotalGB = {ram_info.total / 1024 / 1024 / 1024: .2}
-    ramAvailableGB = {ram_info.available / 1024 / 1024 / 1024: .2}
-    ramUsedGB = {ram_info.used / 1024 / 1024 / 1024}
-    ramUsageValue = ram_info.percent
-    print(ramUsageValue)
-    ramPub.publish(ramUsageValue)
+    def __init__(self):
+        self.pubCpu.publish(self.cpuUsage())
+        self.pubGpu.publish(self.gpuUsage())
+        self.pubRam.publish(self.ramUsage())
+        self.pubScvIP.publish(self.checkSCVIP())
+        self.hunterStatusSub = rospy.Subscriber('/hunter_status', HunterStatus, self.callback)
 
-def checkSCVIP():
-    # self.ipAddressValue = ipAddressValue
-    hostname = socket.gethostname()
-    ipAddressValue = socket.gethostbyname(hostname)
-    print(hostname, ":", ipAddressValue)
-    scvIPPub.publish(ipAddressValue)
+    def gpuUsage(self):
+        nvidia_smi.nvmlInit()
+        deviceCount = nvidia_smi.nvmlDeviceGetCount()
+        for i in range(deviceCount):
+            handle = nvidia_smi.nvmlDeviceGetHandleByIndex(i)
+            util = nvidia_smi.nvmlDeviceGetUtilizationRates(handle)
+            mem = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
+            print(
+                f"|Device {i}| Mem Free: {mem.free / 1024 ** 2:5.2f}MB / {mem.total / 1024 ** 2:5.2f}MB "
+                f"| gpu-util: {util.gpu / 100.0:3.1%} | gpu-mem: {util.memory / 100.0:3.1%} |")
+            gpuUsageValue = util.gpu
+            gpuMempercent = {util.memory / 100.0}
+            print(gpuUsageValue)
+            return gpuUsageValue
 
+    def cpuUsage(self):
+        # self.cpuUsageValue = cpuUsageValue
+        cpuUsageValue = psutil.cpu_percent()
+        print(f"CPU Percentage usage: {cpuUsageValue}%")
+        # CPU frequency
+        # cpu_info = psutil.cpu_freq()
+        # print(f"CPU Current frequency: {cpu_info.current:.2f} Mhz")
+        return cpuUsageValue
 
-# convert json to ros msg
-def checkSCVstatus():
-    floatMsgArr = Float32MultiArray()
+    def ramUsage(self):
+        ram_info = psutil.virtual_memory()
+        print(f"RAM Total: {ram_info.total / 1024 / 1024 / 1024:.2f} GB")
+        print(f"RAM Available: {ram_info.available / 1024 / 1024 / 1024:.2f} GB")
+        print(f"RAM Used: {ram_info.used / 1024 / 1024 / 1024:.2f} GB")
+        print(f"RAM Percentage usage: {ram_info.percent}%")
+        ramTotalGB = {ram_info.total / 1024 / 1024 / 1024: .2}
+        ramAvailableGB = {ram_info.available / 1024 / 1024 / 1024: .2}
+        ramUsedGB = {ram_info.used / 1024 / 1024 / 1024}
+        ramUsageValue = ram_info.percent
+        print(ramUsageValue)
+        return ramUsageValue
 
-    # nodes = rosnode.get_node_names()`
-    # scv_node_name = '/play'
-    # scv_ID = 0
-    #
-    # for node in nodes:
-    #     if(node.startswith(scv_node_name)):
-    #         scv_ID += 1
-    def callback(data):
-        # print(rospy.get_caller_id(), data)
-        floatMsgArr.data = [data.control_mode, data.base_state, data.park_mode, data.battery_voltage, data.linear_velocity, data.steering_angle]
-        scvStatusPub.publish(floatMsgArr)
-    hunterStatusSub = rospy.Subscriber('/hunter_status', HunterStatus, callback)
+    def checkSCVIP(self):
+        # self.ipAddressValue = ipAddressValue
+        hostname = socket.gethostname()
+        ipAddressValue = socket.gethostbyname(hostname)
+        print(hostname, ":", ipAddressValue)
+        return ipAddressValue
 
-# def samplingImg():
-#     def callback(data):
-#         # print(rospy.get_caller_id(), data)
-#         floatMsgArr.data = [data.control_mode, data.base_state, data.park_mode, data.battery_voltage, data.linear_velocity, data.steering_angle]
-#         scvStatusPub.publish(floatMsgArr)
-#     hunterStatusSub = rospy.Subscriber('/hunter_status', HunterStatus, callback)
+    def callback(self, data):
+        floatMsgArr = Float32MultiArray()
+        floatMsgArr.data = [data.control_mode, data.base_state, data.park_mode,
+            data.battery_voltage, data.linear_velocity, data.steering_angle]
+        self.pubScvStatus.publish(floatMsgArr)
 
 
 if __name__ == '__main__':
@@ -92,9 +82,5 @@ if __name__ == '__main__':
     # rospy.spin()
 
     while not rospy.is_shutdown():
-        checkSCVIP()
-        gpuUsage()
-        cpuUsage()
-        ramUsage()
-        checkSCVstatus()
+        scvIn = scvInformation()
         time.sleep(1)
