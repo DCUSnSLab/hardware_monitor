f003d67d60476424ce5f932a5be43e49b804bc89
ParkBoEun <ppos2eun@gmail.com>
2023-11-30 12:50:51 +0900

    GEN-106 launch file 생성 및 rosbridge를 통해 전달되는 메세지 시각화

--- scripts/sendHunter.py
+++ scripts/sendHunter.py
@@ -1,34 +1,100 @@
-#!/usr/env/bin python3
+#!/usr/bin/env python3
 import time
 import rospy
-import std_msgs.msg
+import sensor_msgs.msg
 from std_msgs.msg import *
 from hunter_msgs.msg import *
+import psutil
+import nvidia_smi
+import socket
+import rosnode
 
-pub = rospy.Publisher('/hs', std_msgs.msg.Float64, queue_size=10)
+cpuPub = rospy.Publisher('/cpuPub', Float64, queue_size=10)
+ramPub = rospy.Publisher('/ramPub', Float64, queue_size=10)
+gpuPub = rospy.Publisher('/gpuPub', Float64, queue_size=10)
+scvIPPub = rospy.Publisher('/scvIPPub', String, queue_size=10)
+scvStatusPub = rospy.Publisher('/scvStatusPub', Float32MultiArray, queue_size=10)
+imgSamplingPub = rospy.Publisher('/imgSamplingPub', sensor_msgs.msg.CompressedImage, queue_size=10)
 
-def callback(data):
-    print(data.battery_voltage)
-    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.battery_voltage)
-    voltage = data.battery_voltage
-    pub.publish(voltage)
-    #print(type(data.battery_voltage))
-    #print(type(data))
-    #pub.publish(data)
-    return data.battery_voltage
+def gpuUsage():
+    nvidia_smi.nvmlInit()
+    deviceCount = nvidia_smi.nvmlDeviceGetCount()
+    for i in range(deviceCount):
+        handle = nvidia_smi.nvmlDeviceGetHandleByIndex(i)
+        util = nvidia_smi.nvmlDeviceGetUtilizationRates(handle)
+        mem = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
+        print(
+            f"|Device {i}| Mem Free: {mem.free / 1024 ** 2:5.2f}MB / {mem.total / 1024 ** 2:5.2f}MB "
+            f"| gpu-util: {util.gpu / 100.0:3.1%} | gpu-mem: {util.memory / 100.0:3.1%} |")
+        gpuUsageValue = util.gpu
+        gpuMempercent = {util.memory / 100.0}
+        print(gpuUsageValue)
+        gpuPub.publish(gpuUsageValue)
+
+def cpuUsage():
+    # self.cpuUsageValue = cpuUsageValue
+    cpuUsageValue = psutil.cpu_percent()
+    print(f"CPU Percentage usage: {cpuUsageValue}%")
+    # CPU frequency
+    # cpu_info = psutil.cpu_freq()
+    # print(f"CPU Current frequency: {cpu_info.current:.2f} Mhz")
+    cpuPub.publish(cpuUsageValue)
+
+def ramUsage():
+    # self.ramUsageValue = ramUsageValue
+    ram_info = psutil.virtual_memory()
+    print(f"RAM Total: {ram_info.total / 1024 / 1024 / 1024:.2f} GB")
+    print(f"RAM Available: {ram_info.available / 1024 / 1024 / 1024:.2f} GB")
+    print(f"RAM Used: {ram_info.used / 1024 / 1024 / 1024:.2f} GB")
+    print(f"RAM Percentage usage: {ram_info.percent}%")
+    ramTotalGB = {ram_info.total / 1024 / 1024 / 1024: .2}
+    ramAvailableGB = {ram_info.available / 1024 / 1024 / 1024: .2}
+    ramUsedGB = {ram_info.used / 1024 / 1024 / 1024}
+    ramUsageValue = ram_info.percent
+    print(ramUsageValue)
+    ramPub.publish(ramUsageValue)
+
+def checkSCVIP():
+    # self.ipAddressValue = ipAddressValue
+    hostname = socket.gethostname()
+    ipAddressValue = socket.gethostbyname(hostname)
+    print(hostname, ":", ipAddressValue)
+    scvIPPub.publish(ipAddressValue)
 
 
-def listener():
-    rospy.init_node('talker', anonymous=True)
-    sub = rospy.Subscriber("/hunter_status", HunterStatus, callback)
-    pub = rospy.Publisher('/hs', HunterStatus, queue_size=10)
+# convert json to ros msg
+def checkSCVstatus():
+    floatMsgArr = Float32MultiArray()
 
-    while not rospy.is_shutdown():
-        time.sleep(1)
-        # pub.publish(callback)
+    # nodes = rosnode.get_node_names()`
+    # scv_node_name = '/play'
+    # scv_ID = 0
+    #
+    # for node in nodes:
+    #     if(node.startswith(scv_node_name)):
+    #         scv_ID += 1
+    def callback(data):
+        # print(rospy.get_caller_id(), data)
+        floatMsgArr.data = [data.control_mode, data.base_state, data.park_mode, data.battery_voltage, data.linear_velocity, data.steering_angle]
+        scvStatusPub.publish(floatMsgArr)
+    hunterStatusSub = rospy.Subscriber('/hunter_status', HunterStatus, callback)
+
+# def samplingImg():
+#     def callback(data):
+#         # print(rospy.get_caller_id(), data)
+#         floatMsgArr.data = [data.control_mode, data.base_state, data.park_mode, data.battery_voltage, data.linear_velocity, data.steering_angle]
+#         scvStatusPub.publish(floatMsgArr)
+#     hunterStatusSub = rospy.Subscriber('/hunter_status', HunterStatus, callback)
+
 
 if __name__ == '__main__':
-    try:
-        listener()
-    except rospy.ROSInterruptException:
-        pass
+    rospy.init_node('scvStatus', anonymous=True)
+    # rospy.spin()
+
+    while not rospy.is_shutdown():
+        checkSCVIP()
+        gpuUsage()
+        cpuUsage()
+        ramUsage()
+        checkSCVstatus()
+        time.sleep(1)
--- scripts/usage.py
+++ scripts/usage.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python3
 import psutil
-import os
-
-from tqdm import tqdm
 from time import sleep
-
-# you can calculate percentage of available memory
-print(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total)
-# 20.8
-
-print('memory % used:', psutil.virtual_memory()[2])
-pid = os.getpid()
-python_process = psutil.Process(pid)
-memoryUse = python_process.memory_info()[0]/2.**30*1000  # memory use in GB...I think
-print('memory use:', memoryUse)
+import nvidia_smi
 
 
-with tqdm(total=100, desc='cpu%', position=1) as cpubar, tqdm(total=100, desc='ram%', position=0) as rambar:
-    while True:
-        rambar.n=psutil.virtual_memory().percent
-        cpubar.n=psutil.cpu_percent()
-        rambar.refresh()
-        cpubar.refresh()
-        sleep(0.5)
+def gpuUsage():
+    nvidia_smi.nvmlInit()
+    deviceCount = nvidia_smi.nvmlDeviceGetCount()
+    for i in range(deviceCount):
+        handle = nvidia_smi.nvmlDeviceGetHandleByIndex(i)
+        util = nvidia_smi.nvmlDeviceGetUtilizationRates(handle)
+        mem = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
+        print(
+            f"|Device {i}| Mem Free: {mem.free / 1024 ** 2:5.2f}MB / {mem.total / 1024 ** 2:5.2f}MB "
+            f"| gpu-util: {util.gpu / 100.0:3.1%} | gpu-mem: {util.memory / 100.0:3.1%} |")
+
+def cpuUsage():
+    cpu_percent = psutil.cpu_percent()
+    print(f"CPU Percentage usage: {cpu_percent}%")
+    # CPU frequency
+    cpu_info = psutil.cpu_freq()
+    print(f"CPU Current frequency: {cpu_info.current:.2f} Mhz")
+
+def ramUsage():
+    ram_info = psutil.virtual_memory()
+    print(f"RAM Total: {ram_info.total / 1024 / 1024 / 1024:.2f} GB")
+    print(f"RAM Available: {ram_info.available / 1024 / 1024 / 1024:.2f} GB")
+    print(f"RAM Used: {ram_info.used / 1024 / 1024 / 1024:.2f} GB")
+    print(f"RAM Percentage usage: {ram_info.percent}%")
+
+while True:
+    gpuUsage()
+    cpuUsage()
+    ramUsage()
+
+    sleep(1.5)
+
+
+
+# print(f'gpu: {gpuValue}%, gpu-mem: {res.memory}%')
+# print('RAM memory % used:', psutil.virtual_memory()[2])
+# print("RAM: ", psutil.virtual_memory().percent, "CPU: ", psutil.cpu_percent(0))
+# print("=========================================================================================")
+# print("=========================================================================================")
