d5cee468e517ccb59304c8417bb99930a207db68
ParkBoEun <ppos2eun@gmail.com>
2024-03-06 16:41:37 +0900

    rosservice call complete

--- scripts/sendHunter.py
+++ scripts/sendHunter.py
@@ -7,62 +7,27 @@ import psutil
 import nvidia_smi
 import socket
 from math import pi
-
-# class 예쁘게 만들기
-flag = 0
+from hardware_monitor.srv import AddTwoInts
 
 
 class scvInformation:
+
     flag = 0
     pubCpu = rospy.Publisher('/pubCpu', Float64, queue_size=10)
     pubRam = rospy.Publisher('/pubRam', Float64, queue_size=10)
     pubGpu = rospy.Publisher('/pubGpu', Float64, queue_size=10)
     pubScvIP = rospy.Publisher('/pubScvIP', String, queue_size=10)
-    pubScvStatus = rospy.Publisher('/pubScvStatus', Float64MultiArray, queue_size=10)
+
+    # int8 MOTOR_ID_FRONT = 0
+    # int8 MOTOR_ID_REAR_LEFT = 1
+    # int8 MOTOR_ID_REAR_RIGHT = 2
 
     def __init__(self):
         self.pubCpu.publish(self.cpuUsage())
         self.pubGpu.publish(self.gpuUsage())
         self.pubRam.publish(self.ramUsage())
         self.pubScvIP.publish(self.checkSCVIP())
-        self.hunterStatusSub = rospy.Subscriber('/hunter_status', HunterStatus, self.callback)
-        self.scvMsgArr = Float64MultiArray()
-        self.driver_states_arr = [[0,0,0], [0,0,0], [0,0,0]]
-        self.motor_states_arr = [[0,0,0,0], [0,0,0,0], [0,0,0,0]]
-        # self.driver_states_arr = [[],[],[]]
-        # self.motor_states_arr = [[],[],[]]
-
-
-    def convertToPercent(self, whole, part):
-        return round(part / whole * 100, 2)
-
-    def callback(self, data):
-        global flag
-        for i in range(3):
-            self.driver_states_arr[i] = [data.driver_states[i].driver_voltage,
-                                         data.driver_states[i].driver_temperature,
-                                         data.driver_states[i].driver_state]
-            self.motor_states_arr[i] = [data.motor_states[i].current,
-                                        data.motor_states[i].rpm,
-                                        data.motor_states[i].temperature,
-                                        data.motor_states[i].motor_pose]
-
-        batteryPercent = self.convertToPercent(27.9, data.battery_voltage)
-        # max linear velocity : 1.5 m/s, linear_velocity 값 m/s -> km/s
-        mToKmVelocity = round(data.linear_velocity / 1000, 4)
-        # steering angle + : 왼쪽, - : 오른쪽
-        steeringToRadian = round(data.steering_angle * (180 / pi), 2)
-
-        if flag < 3:
-            flag += 1
-        else:
-            print("batteryVoltage", batteryPercent, "% ", "velocity", mToKmVelocity, "km/s ", "steering angle", steeringToRadian,"˚")
-            flag = 0
-
-        self.scvMsgArr.data = [data.base_state, data.battery_voltage, data.control_mode, data.fault_code, data.linear_velocity, data.park_mode,
-                               data.steering_angle, self.motor_states_arr, self.driver_states_arr]
-        # self.pubScvStatus.publish(self.scvMsgArr)
-
+        # self.hunterStatusSub = rospy.Subscriber('/hunter_status', HunterStatus, self.callback)
 
     def gpuUsage(self):
         nvidia_smi.nvmlInit()
@@ -71,18 +36,18 @@ class scvInformation:
             handle = nvidia_smi.nvmlDeviceGetHandleByIndex(i)
             util = nvidia_smi.nvmlDeviceGetUtilizationRates(handle)
             mem = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
-            print(
-                f"|Device {i}| Mem Free: {mem.free / 1024 ** 2:5.2f}MB / {mem.total / 1024 ** 2:5.2f}MB "
-                f"| gpu-util: {util.gpu / 100.0:3.1%} | gpu-mem: {util.memory / 100.0:3.1%} |")
+            # print(
+            #     f"|Device {i}| Mem Free: {mem.free / 1024 ** 2:5.2f}MB / {mem.total / 1024 ** 2:5.2f}MB "
+            #     f"| gpu-util: {util.gpu / 100.0:3.1%} | gpu-mem: {util.memory / 100.0:3.1%} |")
             gpuUsageValue = util.gpu
             gpuMempercent = {util.memory / 100.0}
-            print(gpuUsageValue)
+            # print(gpuUsageValue)
             return gpuUsageValue
 
     def cpuUsage(self):
         # self.cpuUsageValue = cpuUsageValue
         cpuUsageValue = psutil.cpu_percent()
-        print(f"CPU Percentage usage: {cpuUsageValue}%")
+        # print(f"CPU Percentage usage: {cpuUsageValue}%")
         # CPU frequency
         # cpu_info = psutil.cpu_freq()
         # print(f"CPU Current frequency: {cpu_info.current:.2f} Mhz")
@@ -90,30 +55,40 @@ class scvInformation:
 
     def ramUsage(self):
         ram_info = psutil.virtual_memory()
-        print(f"RAM Total: {ram_info.total / 1024 / 1024 / 1024:.2f} GB")
-        print(f"RAM Available: {ram_info.available / 1024 / 1024 / 1024:.2f} GB")
-        print(f"RAM Used: {ram_info.used / 1024 / 1024 / 1024:.2f} GB")
-        print(f"RAM Percentage usage: {ram_info.percent}%")
+        # print(f"RAM Total: {ram_info.total / 1024 / 1024 / 1024:.2f} GB")
+        # print(f"RAM Available: {ram_info.available / 1024 / 1024 / 1024:.2f} GB")
+        # print(f"RAM Used: {ram_info.used / 1024 / 1024 / 1024:.2f} GB")
+        # print(f"RAM Percentage usage: {ram_info.percent}%")
         ramTotalGB = {ram_info.total / 1024 / 1024 / 1024: .2}
         ramAvailableGB = {ram_info.available / 1024 / 1024 / 1024: .2}
         ramUsedGB = {ram_info.used / 1024 / 1024 / 1024}
         ramUsageValue = ram_info.percent
-        print(ramUsageValue)
+        # print(ramUsageValue)
         return ramUsageValue
 
     def checkSCVIP(self):
         # self.ipAddressValue = ipAddressValue
         hostname = socket.gethostname()
         ipAddressValue = socket.gethostbyname(hostname)
-        print(hostname, ":", ipAddressValue)
+        # print(hostname, ":", ipAddressValue)
         return ipAddressValue
 
+def handle_service(request):
+    result = request.a + request.b
+    rospy.loginfo("Sum: %s", result)
+    return result
 
+def main():
+    rospy.init_node('add_two_ints_server')
+    service = rospy.Service('add_two_ints', AddTwoInts, handle_service)
+    rospy.spin()
 
-if __name__ == '__main__':
-    rospy.init_node('scvStatus', anonymous=True)
-    # rospy.spin()
+if __name__ == "__main__":
+    main()
 
-    while not rospy.is_shutdown():
-        scvIn = scvInformation()
-        time.sleep(1)
+# if __name__ == '__main__':
+#     rospy.init_node('scvStatus', anonymous=True)
+#
+#     while not rospy.is_shutdown():
+#         scvIn = scvInformation()
+#         time.sleep(1)
--- scripts/usage.py
+++ scripts/usage.py
@@ -11,43 +11,43 @@ class usage:
         self.hunterStatusSub = rospy.Subscriber('/hunter_status', HunterStatus, self.callback)
         self.scvMsgArr = Float64MultiArray()
 
-        # self.base_state = 0
-        # self.battery_voltage = 0
-        # self.control_mode = 0
-        # self.fault_code = 0
-        # self.linear_velocity = 0
-        # self.park_mode = 0
-        # self.steering_angle = 0
-        #
-        # self.driver_states_front = []
-        # self.driver_states_rareLeft = []
-        # self.driver_states_rareRight = []
-        #
-        #
-        # self.motor_states_front = []
-        # self.motor_states_rareLeft = []
-        # self.motor_states_rareRight = []
-        #
-        # self.motorStates = [self.motor_states_front, self.motor_states_rareLeft, self.motor_states_rareRight]
-        # self.driverStates = [self.driver_states_front, self.driver_states_rareLeft, self.motor_states_rareRight]
+        self.base_state = 0
+        self.battery_voltage = 0
+        self.control_mode = 0
+        self.fault_code = 0
+        self.linear_velocity = 0
+        self.park_mode = 0
+        self.steering_angle = 0
+
+        self.driver_states_front = []
+        self.driver_states_rareLeft = []
+        self.driver_states_rareRight = []
+
+
+        self.motor_states_front = []
+        self.motor_states_rareLeft = []
+        self.motor_states_rareRight = []
+
+        self.motorStates = [self.motor_states_front, self.motor_states_rareLeft, self.motor_states_rareRight]
+        self.driverStates = [self.driver_states_front, self.driver_states_rareLeft, self.motor_states_rareRight]
         self.driver_states_arr = [[0,0,0], [0,0,0], [0,0,0]]
         self.motor_states_arr = [[0,0,0,0], [0,0,0,0], [0,0,0,0]]
 
     def callback(self, data):
-        # motor_states_front = [data.motor_states[0].current, data.motor_states[0].rpm, data.motor_states[0].temperature, data.motor_states[0].motor_pose]
-        # motor_states_rareLeft = [data.motor_states[1].current, data.motor_states[1].rpm, data.motor_states[1].temperature, data.motor_states[1].motor_pose]
-        # motor_states_rareRight = [data.motor_states[2].current, data.motor_states[2].rpm, data.motor_states[2].temperature, data.motor_states[2].motor_pose]
+        motor_states_front = [data.motor_states[0].current, data.motor_states[0].rpm, data.motor_states[0].temperature, data.motor_states[0].motor_pose]
+        motor_states_rareLeft = [data.motor_states[1].current, data.motor_states[1].rpm, data.motor_states[1].temperature, data.motor_states[1].motor_pose]
+        motor_states_rareRight = [data.motor_states[2].current, data.motor_states[2].rpm, data.motor_states[2].temperature, data.motor_states[2].motor_pose]
 
         for i in range(3):
             self.driver_states_arr[i] = [data.driver_states[i].driver_voltage, data.driver_states[i].driver_temperature, data.driver_states[i].driver_state]
             self.motor_states_arr[i] = [data.motor_states[i].current, data.motor_states[i].rpm, data.motor_states[i].temperature, data.motor_states[i].motor_pose]
 
-        # driver_states_front = [data.driver_states[0].driver_voltage, data.driver_states[0].driver_temperature, data.driver_states[0].driver_state]
-        # driver_states_rareLeft = [data.driver_states[1].driver_voltage, data.driver_states[1].driver_temperature, data.driver_states[1].driver_state]
-        # driver_states_rareRight = [data.driver_states[2].driver_voltage, data.driver_states[2].driver_temperature, data.driver_states[2].driver_state]
+        driver_states_front = [data.driver_states[0].driver_voltage, data.driver_states[0].driver_temperature, data.driver_states[0].driver_state]
+        driver_states_rareLeft = [data.driver_states[1].driver_voltage, data.driver_states[1].driver_temperature, data.driver_states[1].driver_state]
+        driver_states_rareRight = [data.driver_states[2].driver_voltage, data.driver_states[2].driver_temperature, data.driver_states[2].driver_state]
 
-        # motorStates = [motor_states_front, motor_states_rareLeft, motor_states_rareRight]
-        # driverStates = [self.driver_states_front, self.driver_states_rareLeft, self.driver_states_rareRight]
+        motorStates = [motor_states_front, motor_states_rareLeft, motor_states_rareRight]
+        driverStates = [self.driver_states_front, self.driver_states_rareLeft, self.driver_states_rareRight]
 
         self.scvMsgArr.data = [data.base_state, data.battery_voltage, data.control_mode, data.fault_code, data.linear_velocity,
                                data.park_mode, data.steering_angle, self.motor_states_arr, self.driver_states_arr]
