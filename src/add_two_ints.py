2f65759f86a77bc5934a6270db7d8823c9d6a264
ParkBoEun <ppos2eun@gmail.com>
2024-03-08 23:06:29 +0900

    test

--- src/add_two_ints.py
+++ src/add_two_ints.py
@@ -1,17 +1,40 @@
+#!/usr/bin/env python3
 import rospy
-# from rospy_tutorials.srv import AddTwoInts
-from hardware_monitor.srv import AddTwoInts
+from hardware_monitor.srv import Logging
+import os
+import signal
 
-def handle_add_two_ints(req):
-    result = req.a + req.b
-    rospy.loginfo("Sum of " + str(req.a) + " and " + str(req.b) + " is " + str(result))
-    return result
+class rosbagLoggingServer:
+    # 시작 받으면 subprocess.Popen
+    # stop 받으면 pro.pid 받아와서 kill하기
+    def __init__(self):
+        self.processPID = 0
+        self.service = rospy.Service("/logging", Logging, self.handle_add_two_ints)
+
+    def handle_add_two_ints(self, req):
+        result = req.isLogging
+        if str(result) == "LoggingStart":
+            rospy.loginfo("LoggingStart")
+            cmd = "rosbag record -a"
+            os.system(cmd)  # returns the exit code in unix
+            self.processPID = os.getpid()
+            return str(result)
+
+        elif str(result) == "LoggingStop":
+            rospy.loginfo("LoggingStop")
+            rospy.loginfo(self.processPID)
+            os.kill(self.processPID, signal.SIGINT)
+            return str(result)
+
+# service call 특정 문자열을 받으면 rosbag record -a
+# 종료
 
 if __name__ == '__main__':
-    rospy.init_node("add_two_ints_server")
-    rospy.loginfo("Add two ints server node created")
+    rospy.init_node("Logging_Server")
 
-    service = rospy.Service("/add_two_ints", AddTwoInts, handle_add_two_ints)
-    rospy.loginfo("Service server has been started")
+    while not rospy.is_shutdown():
+        rospy.loginfo("Logging Server start")
+        handle = rosbagLoggingServer()
+        rospy.loginfo("Service server has been started")
 
-    rospy.spin()
+        rospy.spin()
---
+++ src/vehicleControl.py
@@ -0,0 +1,49 @@
+import rospy
+from hardware_monitor.srv import VehicleControl
+from geometry_msgs.msg import Twist
+import threading
+
+
+def handle_vehicle_control(req):
+    msg = Twist()
+
+    result = req.scvStatus
+
+    startMsg = Twist()
+    startMsg.linear.x = 0.0
+    startMsg.linear.y = 0.0
+    startMsg.linear.z = 0.0
+    startMsg.angular.x = 0.0
+    startMsg.angular.y = 0.0
+    startMsg.angular.z = 0.0
+
+    stopMsg = Twist()
+    stopMsg.linear.x = 0.5
+    stopMsg.linear.y = 0.0
+    stopMsg.linear.z = 0.0
+    stopMsg.angular.x = 0.0
+    stopMsg.angular.y = 0.0
+    stopMsg.angular.z = 0.0
+
+    if str(result) == "scvStart":
+        rospy.loginfo("scvStart")
+        pub.publish(startMsg)
+        return str(result)
+
+    elif str(result) == "scvStop":
+        rospy.loginfo("scvStop")
+        pub.publish(stopMsg)
+        return str(result)
+
+    else:
+        return 0
+
+if __name__ == '__main__':
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+    rospy.init_node("SCV_Control_Server")
+    rospy.loginfo("Vehicle Control Server start")
+    service = rospy.Service("/scvControlServer", VehicleControl, handle_vehicle_control)
+    rospy.loginfo("Service server has been started")
+
+    rospy.spin()
